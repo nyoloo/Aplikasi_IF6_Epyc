75fbb40b92b20dc4b39c1960fda3b69db65f9ce4
nyoloo <87368516+nyoloo@users.noreply.github.com>
2022-02-12 10:50:50 +0700

    Interface Login

--- pelanggan/login/login.php
+++ pelanggan/login/login.php
@@ -7,67 +7,77 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Delichip Cake Store</title>
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<style>
+    .btn-group{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
+<body class="bg-white">
 
-<div class="div-main">
-    
-        <section class="sign-in">
-            <div class="container">
-<center>            <?php
-if (isset($_GET["error"])) {
-$error = $_GET["error"];
-if ($error == 1) {
-showError("Email dan password tidak sesuai."); }
-else if ($error == 2)
-showError("Error database. Silahkan hubungi administrator");
-else if ($error == 3)
-showError("Koneksi ke Database gagal. Autentikasi gagal.");
-else if ($error == 4)
-showError("Anda tidak boleh mengakses halaman sebelumnya karena belum login.
-Silahkan login terlebih dahulu.");
-else
-showError("Unknown Error.");
-}
-?></center>
-
-                <div class="signin-content">
-                    <div class="signin-image">
-                        <figure><img src="../images/31-315783_kue-ulang-tahun-animasi-clipart.png" alt="sing up image"></figure>
-                        <a href="register.php" class="signup-image-link">Create an account</a>
-                    </div>
-
-                    <div class="signin-form">
-                        <h2 class="form-title">Login</h2>
-                        <form action="logindatabase.php" method="post">
-                            <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"required>
+<div class="container">
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/859080381517266944/865996965488754728/WhatsApp_Image_2021-07-03_at_12.23.31.jpeg" style="width: 10rem;" class="mt-5">
+            <h1>White Rent Car</h1>
+            <div class="card mt-4  shadow p-3 mb-5 bg-white rounded" style="width: 45%;">
+                <div class=" card-body">         
+                    <?php
+                    if (isset($_GET["error"])) {
+                    $error = $_GET["error"];
+                    if ($error == 1) {
+                    showError("Email dan password tidak sesuai."); }
+                    else if ($error == 2)
+                    showError("Error database. Silahkan hubungi administrator");
+                    else if ($error == 3)
+                    showError("Koneksi ke Database gagal. Autentikasi gagal.");
+                    else if ($error == 4)
+                    showError("Anda tidak boleh mengakses halaman sebelumnya karena belum login.
+                    Silahkan login terlebih dahulu.");
+                    else
+                    showError("Unknown Error.");
+                    }
+                    ?>
+                    <div class="mt-3">
+                    <form action="logindatabase.php" method="post">
+                            <table class="table table-borderless">
+                                <tr>
+                                    <td>
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="Your Email"required>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                    <input type="password" name="your_pass" id="your_pass" class="form-control" placeholder="Password"required >
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="btn-group">
+                                        <button type="button" class="btn btn-link"><a href="register.php">Create an account</a></button>
+                                        <input type="submit" name="signin" id="signin" class="btn btn-primary btn-sm" value="Login" >
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                <td align="center"><a href="lupapass.php" class="term-service">lupa Password?</a></td>
+                                </tr>
+                                <div class="form-group">
+                                <a href="#" class="term-service">Login Sebagai owner</a>
                             </div>
-                            <div class="form-group">
-                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"required >
-                            </div>
-                            <div class="form-group">
-                                <a href="lupapass.php" class="term-service">lupa Password?</a></label>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Login"/>
-                            </div>
+                            </table>
                         </form>
-                        
                     </div>
+                    </div>
+        </center>
                 </div>
             </div>
         </section>
     </div>
+    
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
--- pelanggan/login/register.php
+++ pelanggan/login/register.php
@@ -8,47 +8,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Delichip Cake Store</title>
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-white">
 
-<div class="div-main">
-    <section class="signup">
-            <div class="container">
-                <div class="signup-content">
-                    <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form name="regisform" action="registrasi_simpan.php" method="post">
-                            <div class="form-group">
-                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="nama_pelanggan" id="nama_pelanggan" placeholder="Your Name"required>
+<div class="container">
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/859080381517266944/865996965488754728/WhatsApp_Image_2021-07-03_at_12.23.31.jpeg" style="width: 10rem;" class="mt-5">
+            <h1>White Rent Car</h1>
+            <div class="card mt-4  shadow p-3 mb-5 bg-white rounded" style="width: 45%;">
+                <div class=" card-body">         
+                    <div class="mt-3">
+                    <form action="logindatabase.php" method="post">
+                            <table class="table table-borderless">
+                                <tr>
+                                    <td>
+                                    <input type="text" name="nama_pelanggan" id="nama_pelanggan" class="form-control" placeholder="Your Name"required>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="Your Email"required>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                    <input type="password" name="your_pass" id="your_pass" class="form-control" placeholder="Password"required >
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="right">
+                                    <input type="submit" name="signup" id="signup" class="form-submit btn btn-primary" value="Register" >
+                                    </td>
+                                </tr>
+                                <tr>
+                                </tr>
+                                <div class="form-group">
+                                <a href="login.php" class="signup-image-link">Sudah Punya Akun ?</a>
                             </div>
-                            <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"required>
-                            </div>
-                            <div class="form-group">
-                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password" id="password" placeholder="Password"required>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
-                            </div>
+                            </table>
                         </form>
                     </div>
-                    <div class="signup-image">
-                        <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
-                        <a href="login.php" class="signup-image-link">I am already member</a>
                     </div>
+        </center>
                 </div>
             </div>
         </section>
-</div>
+    </div>
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="js/main.js"></script>
 
