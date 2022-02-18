070b18131af8389141555ae6409c4e0f9b714bdb
bijiman <joyss234@gmail.com>
2022-02-18 14:16:58 +0700

    about

--- pelangan/about.php
+++ pelangan/about.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 
-    <title>Delichip Cake Store</title>
+    <title>About</title>
   </head>
   
   <body style="background-color: #F7F6F2 ;">
