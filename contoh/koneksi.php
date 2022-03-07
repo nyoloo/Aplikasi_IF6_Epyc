b563d1f1d6b2fb2299b11073757db70d470ccb75
nyoloo <87368516+nyoloo@users.noreply.github.com>
2022-03-07 19:01:39 +0700

    update ui
    update ui index.php and pelangan/mobil.php

--- contoh/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbUser = 'localhost';
-	$dbHost = 'root';
-	$dbPass = '';
-	$dbName = 'blog_crud';
-
-	$db = mysqli_connect($dbUser, $dbHost, $dbPass, $dbName);
-?>
--- pelangan/mobil.php
+++ pelangan/mobil.php
@@ -1,119 +1,131 @@
 <?php
     include_once("../function.php");
     $halaman = 6; //batasan halaman
-    $db= new database();
+    $db=new database();
 	  $page = isset($_GET['halaman'])? (int)$_GET["halaman"]:1;
 	  $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
 	  $total = $db->__construct()->query("select * from mobil");
 	  $pages = ceil($total->num_rows/$halaman);
     $dataMobil = query("SELECT * FROM mobil limit $mulai,$halaman");
-
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="<a class="vglnk href="https://fonts.googleapis.com/css2?family=Roboto" rel="nofollow"><span>https</span><span>://</span><span>fonts</span><span>.</span><span>googleapis</span><span>.</span><span>com</span><span>/</span><span>css2</span><span>?</span><span>family</span><span>=</span><span>Roboto</span></a>:ital@0;1&display=swap" rel="stylesheet"> 
+        <!--========== BOX ICONS ==========-->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
 
-    <title>White Rent Car | Nama dan Harga Mobil</title>
-  </head>
-  
-  <body style="background-color: #F7F6F2 ;">
-<header class="header">
+        <!--========== CSS ==========-->
+        <link rel="stylesheet" href="mobil/css/styles.css">
+        <link rel="stylesheet" href="style.css">
 
-    <a href="#" class="logo">
-    <img src="https://cdn.discordapp.com/attachments/899880655465357315/943891619692818472/Group_4.png" alt="">
-    </a>
-
-    <nav class="navbar">
-        <ul class="nav justify-content-center">
-            <li class="nav-item">
-              <a class="nav-link active " href="../index.php"><i class="fa fa-home"></i></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.php">Tentang Kami</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="mobil.php">Nama Dan Harga Mobil</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link"data-bs-toggle="modal" data-bs-target="#modal" href="#">Kontak</a>
-            </li>
-          </ul>
-          
-    </nav>
-    <div class="icons">
-      <a class="nav-link  fa fa-user" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="login/login.php">Log In</a></li>
-          </ul>
-      <div class="fas fa-bars" id="menu-btn"></div>
-  </header>
-<br>
-<br>
-<section id="galeri">
-  <div class="container">
-    <div class="row text-center">
-      <div class="col">
-        <h3>
-          Nama dan Harga Sewa Mobil
-        </h3>
-      </div>
-    </div>
-    <div class="row">
-        <?php foreach($dataMobil as $data) :?>
-        <div class="col-md-4 mb-3">
-            <div class="card" style="width: 18rem;">
-            <img src="<?= $data['img_url']; ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $data['nama_mobil'];?></h5>
-                        <p class="card-text">Harga Sewa <br>Rp. <?=number_format($data['harga_sewa'],0,",",".") ?> / Hari</p>
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#modal"class="btn btn-primary">Sewa Sekarang</a>
-                    </div>
-            </div>
-        </div>
-      <?php endforeach; ?> 
-      <center>
-      <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-            <div class="pagination:container">
-        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-            <div class="btn-group me-2" role="group" aria-label="First group">
-            <?php for ($i=1; $i<=$pages ; $i++){ ?>
-                <a class="btn btn-primary"  href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a>
-                <?php } 
-                ?>
+        <title>White Rent Car | Harga Sewa</title>
+    </head>
     
-            </div>
-      </div>
-    </div>
-  </div>
-      </center>
-</section>
-<!-- Modal -->
-<div class="modal fade" id="modal" tabindex="-1" >
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-body">
-                Login terlebih dahulu! <br>Bisa klik icon <i class="fa fa-user"></i> atau klik tombol Log In.
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <a href="login/login.php" class="btn btn-primary">Log In</a>
-                </div>
-              </div>
-            </div>
-  </div>
+    <body>
 
+        <!--========== SCROLL TOP ==========-->
+        <a href="#" class="scrolltop" id="scroll-top">
+            <i class='bx bx-chevron-up scrolltop__icon'></i>
+        </a>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
-  </body>
+        <!--========== HEADER ==========-->
+        <header class="l-header" id="header">
+            <nav class="nav bd-container">
+                <a href="#" class="nav__logo">White Rent Car</a>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="../index.php" class="nav__link">Home</a></li>
+                        <li class="nav__item"><a href="../index.php" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Harga Sewa</a></li>
+                        <li class="nav__item"><a href="login/login.php" class="nav__link">login</a></li>
+
+                        <li><i class='bx bx-moon change-theme' id="theme-button"></i></li>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-menu'></i>
+                </div>
+            </nav>
+        </header>
+
+        <main class="l-main">
+            
+
+            <!--========== MENU ==========-->
+            <section class="menu section bd-container" id="menu">
+                <h2 class="section-title">Harga dan Nama Mobil</h2>
+                
+                <div class="menu__container bd-grid">
+                <?php foreach($dataMobil as $data) :?>
+                    <div class="menu__content">
+                    <img src="<?= $data['img_url']; ?>" alt="" class="menu__img">
+                        <h3 class="menu__name"><?= $data['nama_mobil'];?></h3>
+                        <span class="menu__preci">Harga Sewa <br>Rp. <?=number_format($data['harga_sewa'],0,",",".") ?> / Hari</span>
+                        <br>
+                    </div>
+                    <?php endforeach; ?> 
+                </div>
+                    <div class="menu__pagination">
+                    <center>
+                            <?php for ($i=1; $i<=$pages ; $i++){ ?>
+                                <a class="nav__link"  href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a>
+                            <?php } 
+                                ?>
+                            
+                    </center>
+                    </div>
+                    
+       
+            </section>
+
+        </main>
+
+        <!--========== FOOTER ==========-->
+        <footer class="footer section">
+            <div class="footer__container container grid">
+                <div class="footer__content grid">
+                <div class="footer__rights">
+                    <p class="footer__copy">&#169; 2021 Bedimcode. All rigths reserved.</p>
+                </div>
+            </div>
+            </div>
+        </footer>
+        <!--========== SCROLL REVEAL ==========-->
+        <script src="https://unpkg.com/scrollreveal"></script>
+
+        <!--========== MAIN JS ==========-->
+        <script src="mobil/js/main.js"></script>
+        <script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+    </body>
 </html>
